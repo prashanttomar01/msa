236a0d7979195be6b7cba4140f50c02162bc64bb
prashanttomar01 <prashanttomar.dev@gmail.com>
2025-09-25 13:56:04 +0530

    Modified

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-require_once "/../includes/common_functions.php";
-// require_once "includes/common_functions.php";
+require_once "includes/common_functions.php";
 if (!isset($_SESSION['user_id'])) {
     header('location: login.php');
 }
